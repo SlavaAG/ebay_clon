c0924f9cd03590fccc49190df2543927ffc0a96c
Slava Grachev <slavasig@mail.ru>
2019-04-02 18:42:19 +0300

    пока это все

---
+++ ebay_content.py
@@ -0,0 +1,32 @@
+import requests
+
+from bs4 import BeautifulSoup
+
+def get_html(url):
+    try:
+        result = requests.get(url)
+        result.raise_for_status()
+        return result.text
+
+    except(requests.RequestException, ValueError):
+        return False
+
+def get_ebay_products():
+    html = get_html("https://ru.ebay.com/b/Smart-Watches/178893/bn_152365")
+    if html:
+        soup = BeautifulSoup(html, 'html.parser')
+        all_products = soup.find('ul', class_='b-list__items_nofooter').findAll('li')
+        products_list = []
+        for product in all_products:
+            title = product.find('h3').text
+            url = product.find('a')['href']
+            try:
+                image = product.find('img')['data-src']
+            except KeyError:
+                image = product.find('img')['src']
+            products_list.append({
+                "title": title,
+                "url": url,
+                "image": image
+            })
+        return products_list
